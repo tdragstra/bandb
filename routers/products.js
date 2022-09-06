c4d5bf3b5aee38f1487c7264db8db08374252086
Aggio98 <nigelrizzo@gmail.com>
2022-09-06 14:10:02 +0200

    testing new db

--- routers/products.js
+++ routers/products.js
@@ -6,23 +6,24 @@ const Categorie = require("../models").categorie;
 const router = new Router();
 
 router.get("/", async (req, res, next) => {
-	// should return a list of all the teams
-	const products = await Product.findAll({
-		include: [Categorie],
-	});
-	res.send(products);
+  // should return a list of all the teams
+  const products = await Product.findAll({
+    include: [Categorie],
+  });
+  res.send(products);
 });
 
 router.get("/:id", async (req, res, next) => {
-	// should return a list of products and its categorie
-	try {
-		const product = await Product.findByPk(req.params.id, {
-			include: [Categorie],
-		});
-		res.send(product);
-	} catch (error) {
-		console.log(error);
-	}
+  // should return a list of products and its categorie
+  //   console.log("teste");
+  try {
+    const product = await Product.findByPk(req.params.id, {
+      include: [Categorie],
+    });
+    res.send(product);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 module.exports = router;
